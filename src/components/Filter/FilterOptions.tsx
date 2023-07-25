1a441baf3c5a4d0aa851883ba92dea392af7b62d
Gabriel Lucas <gabthedeveloper@gmail.com>
2023-07-25 18:25:36 -0300

    Atualizado o estilo do filtro

--- src/components/Filter/FilterOptions.tsx
+++ src/components/Filter/FilterOptions.tsx
@@ -2,9 +2,12 @@ import React from 'react';
 import Styles from './Filter.module.css';
 import { FilterData, FilterStateProps } from "../../types/Filter";
 import { GlobalContext } from '../../GlobalContext';
+import Contract from '../Icons/Expand';
+import Expand from '../Icons/Contract';
 
 const FilterOptions = <U extends keyof FilterStateProps>({ dataKey, title, options }: FilterData<U>) => {
   const { filter, setFilter } = React.useContext(GlobalContext);
+  const [toggleFilter, setToggleFilter] = React.useState<boolean>(true);
 
   function handleFilter(e: React.ChangeEvent<HTMLInputElement>, id: string): void {
     const updateFilter = {...filter};
@@ -20,14 +23,20 @@ const FilterOptions = <U extends keyof FilterStateProps>({ dataKey, title, optio
   }
 
   return (
-    <div>
-      <h3>{title}</h3>
-      {options.map((option) => (
-        <div key={option.id} className={Styles.option}>
-          <input type="checkbox" checked={filter[dataKey].includes(option.id)} onChange={(e) => handleFilter(e, option.id)}/>
-          <label>{option.name}</label>
-        </div>
-      ))}
+    <div className={Styles.filteroptions}>
+      <div onClick={() => setToggleFilter(!toggleFilter)}>
+        <h3>{title}</h3>
+        {toggleFilter ? <Contract />  : <Expand />}
+      </div>
+
+      {toggleFilter && (<div className={Styles.listoptions}>
+        {options.map((option) => (
+          <div key={option.id} className={Styles.option}>
+            <input type="checkbox" checked={filter[dataKey].includes(option.id)} onChange={(e) => handleFilter(e, option.id)}/>
+            <label>{option.name}</label>
+          </div>
+        ))}
+      </div>)}
     </div>
   )
 }
